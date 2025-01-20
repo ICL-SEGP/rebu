d304556c296c925f97e4444cfdd936d0e7251345
AJReade <74275743+AJReade@users.noreply.github.com>
2025-01-20 16:28:34 +0000

    added bcrypt and styler formatter

--- rebu_web_api/mix.exs
+++ rebu_web_api/mix.exs
@@ -58,7 +58,9 @@ defmodule RebuWebApi.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:guardian, "~> 2.3"}
+      {:guardian, "~> 2.3"},
+      {:bcrypt_elixir, "~> 3.2"},
+      {:styler, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
