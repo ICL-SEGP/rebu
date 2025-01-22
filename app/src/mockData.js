c0126a606b228baac0d53f5371474bbcf8bf1689
roypark337 <roypark337@users.noreply.github.com>
2025-01-22 21:46:03 +0000

    embed mock data directly to frontend

---
+++ app/src/mockData.js
@@ -0,0 +1,15 @@
+// src/mockData.js
+
+const mockData = {
+    tokens: {
+      available: 500,
+      locked: 200,
+    },
+    transactions: [
+      { date: '2025-01-01', item: 'Product A', price: 100 },
+      { date: '2025-01-15', item: 'Product B', price: 200 },
+    ],
+  };
+  
+  export default mockData;
+  
