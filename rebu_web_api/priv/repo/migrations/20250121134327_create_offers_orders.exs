f3014615efd68ee381ca9a32ef1131eb6825afac
AJReade <74275743+AJReade@users.noreply.github.com>
2025-01-22 01:15:31 +0000

    offers orders many to many (potentially a order could have doubled up offers)

---
+++ rebu_web_api/priv/repo/migrations/20250121134327_create_offers_orders.exs
@@ -0,0 +1,12 @@
+defmodule RebuWebApi.Repo.Migrations.CreateOffersOrders do
+  use Ecto.Migration
+
+  def change do
+    create table(:offers_orders) do
+      add :offer_id, references(:offers)
+      add :order_id, references(:orders)
+    end
+
+    create unique_index(:offers_orders, [:offer_id, :order_id])
+  end
+end
