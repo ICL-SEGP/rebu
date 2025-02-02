b0934290303d622ac90b0224522871d1f82aa9c2
AJReade <74275743+AJReade@users.noreply.github.com>
2025-02-02 20:35:34 +0000

    set up gh secrets using terraform

---
+++ rebu_web_api/environments/production/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key_id" {
+  description = "The AWS access key"
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  description = "The AWS secret access key"
+  type = string
+}
+
+variable "age_key" {
+  description = "Age decryption key"
+  type = string
+}
+
+variable "gh_pat" {
+  description = "gh pat"
+  type = string
+}
