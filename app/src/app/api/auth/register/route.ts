b8fd42fcece45bb8793cc1c2daed3b23f4c43a87
AJReade <74275743+AJReade@users.noreply.github.com>
2025-02-03 14:32:41 +0000

    extracted api route as a constant and updated paths

--- app/src/app/api/auth/register/route.ts
+++ app/src/app/api/auth/register/route.ts
@@ -1,11 +1,35 @@
 import { NextResponse } from "next/server";
+import { API_BASE_URL } from "@/app/constants"
 
-export async function POST(/*req: Request*/) {
-  //const { email, password, name } = await req.json();
+export async function POST(req: Request) {
+  const { password, email, first_name, last_name} = await req.json();
 
-  // Add your logic to store user credentials in a database
-  // For example, insert into a users table
-  //const user = { email, password, name };
+  // Sends request to backend.
+  try {
+    const res = await fetch(`${API_BASE_URL}/api/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ password, email, first_name, last_name }),
+    });
+
+    // Check if the response is ok
+    if (!res.ok) {
+      const errorText = await res.text(); // Get the error details as text
+      console.error("Server error:", errorText);
+
+      return NextResponse.json({ message: "Server error" }, { status: 400 });
+    }
+
+    // Parse the JSON response
+    const data = await res.json();
+    console.log(data)
+    return NextResponse.json({ message: "User registered successfully." }, { status: 201 });
+
+  } catch (error) {
+    console.error("Error during fetch:", error);
+  }
+
+  return NextResponse.json({ message: "Registration failed." }, { status: 400 });
 
   // If registration is successful
   return NextResponse.json({ message: "User registered successfully." }, { status: 201 });
---
+++ app/src/app/constants.ts
@@ -0,0 +1,17 @@
+export const API_BASE_URL = "http://176.34.210.163:4000";
+
+// export const ROUTES = {
+//   HOME: "/",
+//   ABOUT: "/about",
+//   CONTACT: "/contact",
+// };
+
+// export const APP_CONFIG = {
+//   APP_NAME: "My Next.js App",
+//   VERSION: "1.0.0",
+// };
+
+// export const ERROR_MESSAGES = {
+//   GENERIC_ERROR: "Something went wrong. Please try again.",
+//   NOT_FOUND: "The requested resource was not found.",
+// };
