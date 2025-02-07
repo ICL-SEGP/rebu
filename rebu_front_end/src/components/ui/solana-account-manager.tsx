355cddb3cb4d8e77bfdd485b39e71bfe83e7e1da
Gabriel Costa <gabe.c.f.m.costa@gmail.com>
2025-02-07 12:48:06 +0000

    Added some wallet functionality.

---
+++ rebu_front_end/src/components/ui/solana-account-manager.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useConnection } from "@solana/wallet-adapter-react";
+import { program, configPDA, ConfigAccount } from "@/anchor/setup";
+
+// export function newAccount() {
+
+// }
