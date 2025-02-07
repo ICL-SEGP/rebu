963fba986fc5b5ba5f03e37997b59738576054f7
AJReade <74275743+AJReade@users.noreply.github.com>
2025-02-07 11:17:41 +0000

    convert order create date to native time before returning in json view

--- rebu_web_api/lib/rebu_web_api_web/controllers/views/order_json.ex
+++ rebu_web_api/lib/rebu_web_api_web/controllers/views/order_json.ex
@@ -6,17 +6,18 @@ defmodule RebuWebApiWeb.OrderJSON do
   Renders a list of orders.
   """
   def index(%{orders: orders}) do
-    %{data: Enum.map(orders, &serialize_order/1)}
+    %{orders: Enum.map(orders, &serialize_order/1)}
   end
 
   @doc """
   Renders a single order.
   """
   def show(%{order: order}) do
-    %{data: serialize_order(order)}
+    serialize_order(order)
   end
 
   defp serialize_order(%Order{} = order) do
+    order = Map.put(order, :inserted_at, Calendar.strftime(DateTime.to_naive(order.inserted_at), "%d %B %Y %H:%M:%S"))
     order_map = JSONHelpers.serialize_schema(order)
 
     # Handle relationships if preloaded, otherwise return nil
