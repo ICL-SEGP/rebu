3dd5629d91346574e04c71d6829b6486ca55a5a8
Gabriel Costa <gabe.c.f.m.costa@gmail.com>
2025-02-20 18:14:34 +0000

    Added get_balance functionality.

--- rebu_web_api/native/solana_api/src/lib.rs
+++ rebu_web_api/native/solana_api/src/lib.rs
@@ -2,4 +2,4 @@ pub mod api_funcs;
 
 pub use api_funcs::*;
 
-rustler::init!("Elixir.RebuWebApi.SolanaApi", [mint_tokens_to_user]);
+rustler::init!("Elixir.RebuWebApi.SolanaApi", [mint_tokens_to_user, get_user_token_balance]);
