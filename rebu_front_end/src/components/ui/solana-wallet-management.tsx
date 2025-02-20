abd20005836358fed6de68f6bf49cd13b690ac4d
AJReade <74275743+AJReade@users.noreply.github.com>
2025-02-20 17:44:15 +0000

    pushing public key to the server

---
+++ rebu_front_end/src/components/ui/solana-wallet-management.tsx
@@ -0,0 +1,50 @@
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletButton } from '@/components/ui/solana-provider'
+import { API_BASE_URL } from '@/lib/constants';
+import { useSession } from 'next-auth/react';
+
+export default function WalletSolana() {
+  const { publicKey } = useWallet()
+  const { data: session } = useSession();
+
+  const pushKey = async (key) => {
+
+    if (!session) {
+      return
+    }
+
+    try {
+      const res = await fetch(`${API_BASE_URL}/api/user/publicKey`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+        body: JSON.stringify({
+          publicKey: key
+        }),
+      });
+
+      if (!res.ok) throw new Error("Failed to push key");
+
+      const response = (await res.json());
+
+      console.log(response)
+
+    } catch (error) {
+      console.error("Error pushing key", error);
+    }
+  };
+
+  if (publicKey) {
+    pushKey(publicKey);
+  }
+
+  return (
+    <div className="hero py-[64px]">
+      <div className="hero-content text-center">
+        <WalletButton />
+      </div>
+    </div>
+  )
+}
