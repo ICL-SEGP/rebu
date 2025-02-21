e61bec11720da1cea5582b7d2b66d77ad2e5a9b3
AJReade <74275743+AJReade@users.noreply.github.com>
2025-02-21 12:00:07 +0000

    added item cost to offer

--- rebu_web_api/lib/rebu_web_api/sales/schemas/offer.ex
+++ rebu_web_api/lib/rebu_web_api/sales/schemas/offer.ex
@@ -2,16 +2,36 @@ defmodule RebuWebApi.Sales.Offer do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Jason.Encoder,
+           only: [
+             :id,
+             :desc,
+             :affiliate_link,
+             :rebate_percentage,
+             :item_cost,
+             :status,
+             :offer_start,
+             :offer_end,
+             :admin_id,
+             :inserted_at,
+             :updated_at
+           ]}
+
   schema "offers" do
     field :desc, :string
     field :affiliate_link, :string
     field :rebate_percentage, :decimal
+    field :item_cost, :decimal
+    field :status, Ecto.Enum, values: [:scheduled, :active, :expired], default: :active
     field :offer_start, :naive_datetime
     field :offer_end, :naive_datetime
 
     belongs_to :admin, RebuWebApi.Accounts.Admin
 
-    many_to_many :order, RebuWebApi.Sales.Order, join_through: "offers_orders"
+    many_to_many :order, RebuWebApi.Sales.Order,
+      join_through: "offers_orders",
+      on_delete: :delete_all,
+      on_replace: :delete
 
     timestamps(type: :utc_datetime)
   end
@@ -25,13 +45,17 @@ defmodule RebuWebApi.Sales.Offer do
       :desc,
       :offer_start,
       :offer_end,
+      :status,
+      :item_cost
     ])
+    |> validate_inclusion(:status, [:scheduled, :active, :expired])
     |> validate_required([
       :affiliate_link,
       :rebate_percentage,
       :desc,
       :offer_start,
-      :offer_end
+      :offer_end,
+      :item_cost
     ])
   end
 end
