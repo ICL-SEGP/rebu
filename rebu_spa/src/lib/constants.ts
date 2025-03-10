94538e441b91f9f689f96b3c2bb06ae8ef5319b0
Alex Reade <74275743+AJReade@users.noreply.github.com>
2025-03-10 14:33:38 +0000

    Update constants.ts

--- rebu_spa/src/lib/constants.ts
+++ rebu_spa/src/lib/constants.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "http://18.201.163.141:4000";
+// export const API_BASE_URL = "http://18.201.163.141:4000";
+export const API_BASE_URL = "https://elixir-web-app.ngrok.app";
 // export const API_BASE_URL = "http://localhost:4000";
 export const BLOCKCHAIN_OFFSET = 2000;
