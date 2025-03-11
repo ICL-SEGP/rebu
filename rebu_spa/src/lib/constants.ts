bb4ed5ad9a775eeb66e515184ae74eb34c1f50ab
Alex Reade <74275743+AJReade@users.noreply.github.com>
2025-03-11 20:57:31 +0000

    Update constants.ts to use our domain

--- rebu_spa/src/lib/constants.ts
+++ rebu_spa/src/lib/constants.ts
@@ -1,4 +1,3 @@
-// export const API_BASE_URL = "http://18.201.163.141:4000";
-export const API_BASE_URL = "https://elixir-web-app.ngrok.app";
+export const API_BASE_URL = "https://rebu.online";
 // export const API_BASE_URL = "http://localhost:4000";
 export const BLOCKCHAIN_OFFSET = 2000;
